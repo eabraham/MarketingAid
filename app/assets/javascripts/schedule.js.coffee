bace23ee05bee6f1e447ffe6b7576cb2b0e7482f
Eric Abrahamsen <Eric@Ploonky.com>
2013-02-17 01:11:17 -0500

    UI Cleanup and Job Detail.

--- app/assets/javascripts/schedule.js.coffee
+++ app/assets/javascripts/schedule.js.coffee
@@ -1,3 +1,12 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+
+jQuery ->
+  $('#meetup_event_schedule_date').datepicker
+    dateFormat: "mm-dd-yy",
+  $('#meetup_event_date').datepicker
+    dateFormat: "mm-dd-yy",
+  $('#tweet_schedule_date').datepicker
+    dateFormat: "mm-dd-yy",
+
--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -1,27 +1,34 @@
 class ScheduleController < ApplicationController
   before_filter :authenticate_user!
+  @@dateTimeFormat="%m-%d-%Y %H:%M"
+
   def index
     @meetup_groups=get_meetup_groups
+
+    job_data=Delayed::Job.all.map{|a| [a.payload_object.token,a.payload_object.object.consumer.site, a.run_at]}
+    job_runtime=Delayed::Job.all.reduce{|a,b| ({a.payload_object.token => a.run_at}).merge({b.payload_object.token => b.run_at})}
+    @consumer_token_detail=ConsumerToken.joins('INNER JOIN users ON users.id=consumer_tokens.user_id').where(:token=>job_runtime.keys).where('users.id = ?',current_user.id)
+
+    
   end
   def schedule_meetup_event
-    schedule_time=params['meetup_event_schedule']
+    schedule_time=buildTime(params['meetup_event_schedule_date'],params['meetup_event_schedule_time'],@@dateTimeFormat) 
+    event_time=buildTime(params['meetup_event_date'],params['meetup_event_time'],@@dateTimeFormat)
     group_id=params['meetup_event_group']['meetup_group_id']
     name=params['meetup_event_name']
     desc=params['meetup_event_description']
 
     meetupToken=ConsumerToken.find_by_type('MeetupToken')
-    meetupToken.client.delay({:run_at => schedule_time}).post('https://api.meetup.com/2/event',{:group_id=>group_id,:name=>name,:description=>desc,:time=>""})
-    #delay({:run_at => schedule_time})
+    meetupToken.client.delay({:run_at => schedule_time}).post('https://api.meetup.com/2/event',{:group_id=>group_id,:name=>name,:description=>desc,:time=>event_time.to_i/1000})
 
     redirect_to :action => :index and return
   end
 
   def schedule_tweet
-    schedule_time=params['tweet_schedule']
+    
+    schedule_time=buildTime(params['tweet_schedule_date'],params['tweet_schedule_time'],@@dateTimeFormat)
     tweet=params['tweet']
 
-    puts "#{schedule_time} #{tweet}"
-    
     tweetToken=ConsumerToken.find_by_type('TwitterToken')
     consumer=OAuth::Consumer.new(OAUTH_CREDENTIALS[:twitter][:key],OAUTH_CREDENTIALS[:twitter][:secret],{:site=> "http://api.twitter.com", :scheme=>:header})
     token_hash = {:oauth_token => tweetToken.token, :oauth_token_secret=>tweetToken.secret} 
@@ -41,6 +48,11 @@ private
 
     #return array of group names, ids and current members role
     return c['results'].map{|a| [a['name'],a['id'],a['self']['role']]}.select{|w| w[2]!=nil}
+  end
+private
+  def buildTime(date,time,format)
+    puts "#{date} #{time['(4i)']}:#{time['(5i)']}"
+    Time.strptime("#{date} #{time['(4i)']}:#{time['(5i)']}",format) 
   end
 
 end
